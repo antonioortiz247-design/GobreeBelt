d8b5d5b60bea7aca6d59508d625a62dd74af6a1c
antonioortiz247-design <antonioortiz247@gmail.com>
2026-03-04 17:35:14 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+document.querySelectorAll("nav a").forEach(link=>{
+link.addEventListener("click",function(e){
+
+e.preventDefault()
+
+document.querySelector(this.getAttribute("href"))
+.scrollIntoView({behavior:"smooth"})
+
+})
+})
