eb542fdc8902e6e48196f5c7206bb14e4f7f6a28
antonioortiz247-design <antonioortiz247@gmail.com>
2026-03-04 21:53:03 -0600

    Add lightbox functionality for gallery images

--- script.js
+++ script.js
@@ -9,3 +9,24 @@ document.querySelector(this.getAttribute("href"))
 
 })
 })
+const images = document.querySelectorAll(".gallery-img")
+const lightbox = document.getElementById("lightbox")
+const lightboxImg = document.getElementById("lightbox-img")
+const close = document.querySelector(".close")
+
+images.forEach(img => {
+
+img.addEventListener("click", ()=>{
+
+lightbox.style.display = "flex"
+lightboxImg.src = img.src
+
+})
+
+})
+
+close.addEventListener("click", ()=>{
+
+lightbox.style.display = "none"
+
+})
